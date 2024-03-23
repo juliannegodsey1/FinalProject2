41b22c838bc80bcd6124f332af52c3c92f2f0807
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-03-22 21:14:45 -0400

    added js

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = doctument.querySelector("#new-task-input");
+    const list_element = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if(!task) {
+            alert("Please fill out the task");
+            return;
+        }
+
+        const task_element = document.createElement("div");
+        task_element.classList.add("task");
+
+        const task_content_element = document.createElement("div");
+        task_content_element.classList.add("content");
+
+        task_element.appendChild(task_content_element);
+
+        const task_input_element = document.createElement("input");
+        task_input_element.type = "text";
+        task_input_element.classList.add("text");
+        task_input_element.value = task;
+        task_input_element.setAttribute("readonly", "readonly");
+
+        task_content_element.appendChild(task_input_element);
+
+        list_element.appendChild(task_element);
+
+    }) 
+})
