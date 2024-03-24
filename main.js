6942c2a6e75ba8505c7c236538360638705ed8be
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-03-24 15:21:24 -0400

    changed color scheme

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ window.addEventListener('load', () => {
     const input = document.querySelector("#new-task-input");
     const list_element = document.querySelector("#tasks");
 
+    loadTasks();
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -13,6 +15,41 @@ window.addEventListener('load', () => {
             return;
         }
 
+        const task_element = createTaskElement(task);
+        list_element.appendChild(task_element);
+
+        saveTask(task);
+
+        input.value = "";
+    });
+
+    function loadTasks() {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.forEach(task => {
+            const task_element = createTaskElement(task);
+            list_element.appendChild(task_element);
+        });
+    }
+
+    function saveTask(task) {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function updateTask(oldTask, newTask) {
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks = tasks.map(task => (task === oldTask ? newTask : task)); 
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function removeTask(task) {
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks = tasks.filter(t => t !== task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function createTaskElement(task) {
         const task_element = document.createElement("div");
         task_element.classList.add("task");
 
@@ -45,24 +82,27 @@ window.addEventListener('load', () => {
 
         task_element.appendChild(task_actions_element);
 
-
-        list_element.appendChild(task_element);
-
-        input.value = "";
-
-
         task_edit_element.addEventListener('click', () => {
             if(task_edit_element.innerText.toLowerCase() == "edit") {
                 task_input_element.removeAttribute("readonly");
                 task_input_element.focus();
+                task_input_element.setAttribute("data-old-task", task_input_element.value); 
                 task_edit_element.innerText = "Save";
             } else {
+                const newTask = task_input_element.value;
+                const oldTask = task_input_element.getAttribute("data-old-task"); 
                 task_input_element.setAttribute("readonly", "readonly");
                 task_edit_element.innerText = "Edit";
+                updateTask(oldTask, newTask); 
             }
+        });
+        
 
+        task_delete_element.addEventListener('click', () => {
+            list_element.removeChild(task_element);
+            removeTask(task);
+        });
 
-        })
-
-    }) 
-})
+        return task_element;
+    }
+});
